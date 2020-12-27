27d1345ceda21a1181af90d69f0e4388a590e025
Janne Lammela <n0laja00@students.oamk.fi>
2020-12-27 13:02:39 +0200

    koodit

---
+++ js/janne.js
@@ -0,0 +1,170 @@
+$(document).ready(function(){
+    $("#startData").click(function(){
+        encounter.allCreatures.length = 0;
+        $("#encounterList").html("");
+
+
+        let creature = new Creature("Goblin", 15, 7, 7, 2);
+        encounter.creatureToInit(creature);
+
+        creature = new Creature("Goblin", 15, 7, 7, 20);
+        encounter.creatureToInit(creature);
+
+        creature = new Creature("Goblin", 15, 7, 7, 11);
+        encounter.creatureToInit(creature);
+        
+        print();
+    });
+
+    $("#clear").click(function(){
+        encounter.allCreatures.length = 0;
+        $("#encounterList").html("");
+        currentTurn = 0;
+        currentRound = 1;
+        $("#round").html("");
+        $("#next").addClass("collapse");
+        $("#previous").addClass("collapse");
+        $("#start").removeClass("collapse");
+        $("#end").addClass("collapse");
+        $("#round").addClass("collapse");
+    });
+
+$("#descending").click(function(){
+
+    encounter.allCreatures.sort(function(a, b) {
+        return b._init - a._init;
+    });
+
+
+    print();
+});
+
+$("#ascending").click(function(){
+
+    encounter.allCreatures.sort(function(a, b) {
+        return a._init - b._init;
+    });
+
+
+    print();
+});
+
+$("#closeTab").click(function(){
+    $("#creatureStatTab").addClass("collapse");
+   
+});
+
+$("#closeTab1").click(function(){
+    $("#creatureAdd").toggleClass("collapse");
+    $("#creatureStatTab").addClass("col-5");
+    $("#listTab").addClass("col-7");
+    $("#ascending").after("<button type='button' class='btn btn-secondary col-12' id='openAdd' onclick='openAdd()'>Add Creature</button>");
+});
+
+
+$("#save").click(function(){
+    if ($('input.hostile').is(':checked')) { 
+        let creature = new Creature(
+            $("#name").val(),
+            $("#ac").val(),
+            $("#max-hp").val(),
+            $("#hp").val(),
+            $("#init").val(),
+            1,
+            );
+            hostiles++;
+            console.log(hostiles);
+            encounter.creatureToInit(creature);
+    } else {
+        let creature = new Creature(
+        $("#name").val(),
+        $("#ac").val(),
+        $("#max-hp").val(),
+        $("#hp").val(),
+        $("#init").val(),
+        0,
+        );
+        
+        console.log(hostiles);
+        encounter.creatureToInit(creature);
+    }
+        
+        print();
+});
+
+$("#saveEncounter").click(function(){
+    let encounterNameSaved = $("#encounterName").val();
+
+    let statArrayName = $("#encounterName").val() + "name";
+    let statArrayAc = $("#encounterName").val() +  "ac";
+    let statArrayHp = $("#encounterName").val()  + "hp";
+    let statArrayMaxHp =  $("#encounterName").val() + "maxhp";
+    let statArrayInit = $("#encounterName").val() + "init";
+    let statArrayHostile = $("#encounterName").val() + "hostile";
+    let statArrayConcentration = $("#encounterName").val()+  "concentration";
+
+    let lName = [];
+    let lAc = []
+    let lmaxHp = [];
+    let lHp = [];
+    let lInit = [];
+    let lHostile = [];
+    let lConcentration = [];
+
+    let load = localStorage.getItem("DREncounterTracker");
+    loadEncounters = JSON.parse(load);
+    if(!loadEncounters) {
+        loadEncounters = [];
+    };
+    
+    loadEncounters.push(encounterNameSaved);
+
+    for( let i = 0; i < encounter.allCreatures.length; i++){
+        lName.push(encounter.allCreatures[i].name);
+        lAc.push(encounter.allCreatures[i].ac); 
+        lmaxHp.push(encounter.allCreatures[i].maxhp);
+        lHp.push(encounter.allCreatures[i].hp);
+        lInit.push(encounter.allCreatures[i].init);
+        lHostile.push(encounter.allCreatures[i].hostile);
+        lConcentration.push(encounter.allCreatures[i].lConcentration);
+    };
+
+
+    let JSONName = JSON.stringify(lName);
+    let JSONAc = JSON.stringify(lAc);
+    let JSONMaxHp = JSON.stringify(lmaxHp);
+    let JSONHp = JSON.stringify(lHp);
+    let JSONInit = JSON.stringify(lInit);
+    let JSONHostile = JSON.stringify(lHostile);
+    let JSONConcentration = JSON.stringify(lConcentration);
+    
+    localStorage.setItem(statArrayName, JSONName);
+    localStorage.setItem(statArrayAc, JSONAc);
+    localStorage.setItem(statArrayMaxHp, JSONMaxHp);
+    localStorage.setItem(statArrayHp, JSONHp);
+    localStorage.setItem(statArrayInit, JSONInit);
+    localStorage.setItem(statArrayHostile, JSONHostile);
+    localStorage.setItem(statArrayConcentration, JSONConcentration);
+
+    let JSONSaved = JSON.stringify(loadEncounters);
+    localStorage.setItem("DREncounterTracker", JSONSaved);
+
+    });
+
+    $("#load").click(function(){
+        let load = localStorage.getItem("DREncounterTracker");
+        loadEncounters = JSON.parse(load);
+        if(!loadEncounters) {
+            $("#loadingList").html("<a class='list-group-item list-group-item-action'>You've got no loadable encounters!</a>")
+        };
+        $.each(loadEncounters, function( index, value ) {
+            console.log(loadEncounters);
+            $("#loadingList").append("<a class='list-group-item list-group-item-action' onclick='loadItem(" + index + ")'>" + index + ": " + value + "</a>");
+          });
+    });
+    $('#loadModal').on('hidden.bs.modal', function() {
+        $("#loadingList").html("");
+      });
+
+
+});
---
+++ js/janne1.js
@@ -0,0 +1,279 @@
+
+
+    class Creature {
+        constructor(name, ac, maxhp, hp, init, hostile, concentration) {
+            this._name = name;
+            this._ac = ac;
+            this._maxhp = maxhp;
+            this._hp = hp;
+            this._init = init;
+            this._hostile = hostile;
+            this._concentration = concentration;
+        }
+        printListStat(){
+            return "<div class='row creature-block'>" + "<div class='col-2 initBox align-self-center'>" + this._init +
+             "</div>" + "<div class='col-6 centerBox'>" + this._name + '<br>' + 'AC: ' + this._ac + "</div>" + "<div class='col-4 hpBox'>" + this._hp + "/" + 
+             this._maxhp + "</div>" + "</div>"; 
+        }
+
+   
+
+        set name(cname){
+            this._name = cname;
+        }
+
+        set ac(cac){
+            this._ac = cac;
+        }
+
+        set maxhp(maxchp){
+            this._maxhp = maxchp;
+        }
+
+        set hp(chp){
+            this._hp = chp;
+        }
+
+        set init(cinit){
+            this._init = cinit;
+        }
+
+        set hostile(chostile){
+            this._hostile = chostile;
+        }
+
+        set concentration(cconcentration){
+            this._concentration = cconcentration;
+        }
+
+        get name(){
+            return this._name;
+        }
+
+        get ac(){
+            return this._ac;
+        }
+
+        get maxhp(){
+            return this._maxhp;
+        }
+
+        get hp(){
+            return this._hp;
+        }
+
+        get init(){
+            return this._init;
+        }
+        get hostile(){
+            return this._hostile;
+        }
+        get concentration(){
+            return this._concentration;
+        }
+
+  
+    };
+    let hostiles = 0;
+
+    let encounter = {
+        allCreatures: new Array(),
+        
+
+        creatureToInit: function(creature) {
+            
+            this.allCreatures.push(creature);
+                
+        },
+
+        creatureRemove: function(index) {
+            let creature = this.allCreatures[index]._hostile;
+            if (creature == 1){
+                hostiles--;
+                console.log(hostiles);
+            };
+            this.allCreatures.splice(index, 1);
+        },
+    };
+
+function printOne(index){
+    let printCreature = encounter.allCreatures[index];
+    $("#creatureStatTab").removeClass("collapse");
+    $("#creatureStatTab p").remove();
+    $("#creatureStatTab #edit1").remove();
+    $("#creatureStatTab #remove").remove();
+
+    
+    $("#name1").val(printCreature._name);
+    $("#ac1").val(printCreature._ac);
+    $("#maxhp1").val(printCreature._maxhp);
+    $("#hp1").val(printCreature._hp);
+    $("#init1").val(printCreature._init);
+    if (printCreature.concentration == 1) {
+        $('input.concentration').prop( "checked", true )
+    } else {
+        $('input.concentration').prop( "checked", false )
+    };
+    
+    $("#creatureStatTab #closeTab").before("<button type='button' class='btn btn-success col-12' id='edit1' onclick='edit(" + index + ")'>Save Changes</button>");
+    $("#creatureStatTab").append("<button type='button' class='btn btn-danger col-12 mt-5' id='remove' onclick='remove(" + index + ")'>Remove Creature</button>");
+
+};
+
+
+function print() {
+        
+    $("#encounterList").html("");
+
+    for(let i = 0; i < encounter.allCreatures.length; i++) {
+        let oneCreature = encounter.allCreatures[i];
+        let listedCreature = "creature" + i; 
+        
+        $("#encounterList").append("<span id='creature" + [i] + "'  onclick='printOne(" + i + ")'>" + oneCreature.printListStat() + "</span>");
+        if (oneCreature.hp <= 0) {
+            $("#"+listedCreature+" .row").addClass("dead");
+        }  else if (oneCreature.hp <= oneCreature.maxhp / 2) {
+            $("#"+listedCreature + " .row").addClass("bloodied");
+        };
+        if (oneCreature.concentration == 1) {
+            $("#"+listedCreature + " .row .hpBox").append("<br>" + "C")
+
+        };
+
+    };
+    turns();
+};
+
+function edit(index){
+
+    let editCreature = encounter.allCreatures[index];
+
+    editCreature.name = document.getElementById('name1').value;
+    editCreature.ac = document.getElementById('ac1').value;
+    editCreature.hp = document.getElementById('hp1').value;
+    editCreature.maxhp = document.getElementById('maxhp1').value;
+    editCreature.init = document.getElementById('init1').value; 
+    if ($('input.concentration').is(':checked')) {
+        editCreature.concentration = 1;
+        
+    } else {
+        editCreature.concentration = 0;
+    }
+    print();
+};
+
+function remove(index){
+    encounter.creatureRemove(index);
+    $("#creatureStatTab").toggleClass("collapse");
+    print();
+};
+
+function openAdd(){
+    $("#creatureAdd").toggleClass("collapse");
+    $("#openAdd").remove();
+    $("#creatureStatTab").removeClass("col-5");
+    $("#listTab").removeClass("col-7");
+};
+
+let currentTurn = 0;
+let currentRound = 1;
+
+
+
+function nextTurn(){
+    $("#creature" + currentTurn + " .row").removeClass("current");
+    currentTurn++;
+    if (currentTurn >= encounter.allCreatures.length) {
+        currentTurn = 0;
+        currentRound++;
+    };
+    $("#creature" + currentTurn + " .row").addClass("current");
+    $("#round").html("Round: " + currentRound);
+    
+
+}
+function previousTurn(){
+    $("#creature" + currentTurn + " .row").removeClass("current");
+    currentTurn--;
+    if (currentTurn < 0) {
+        currentTurn = encounter.allCreatures.length;
+        $("#creature" + "0 .row").removeClass("current");
+        currentTurn--;
+        currentRound--;
+        if (currentRound == 0) {
+            currentRound = 1;
+        };
+    }; 
+    $("#creature" + (currentTurn) + " .row").addClass("current");
+    $("#round").html("Round: " + currentRound);
+    
+};
+
+function start(){
+    $("#previous").toggleClass("collapse")
+    $("#next").toggleClass("collapse");
+    $("#start").toggleClass("collapse");
+    $("#end").toggleClass("collapse");
+    $("#round").toggleClass("collapse");
+    $("#round").append("Round: " + currentRound);
+    
+    $("#creature"+currentTurn+" .row").addClass("current");
+};
+function turns(){
+    $("#creature"+currentTurn + " .row").addClass("current");
+};
+
+function end(){
+    currentTurn = 0;
+    currentRound = 1;   
+    $("#round").html("");
+    $("#next").toggleClass("collapse");
+    $("#previous").toggleClass("collapse");
+    $("#start").toggleClass("collapse");
+    $("#end").toggleClass("collapse");
+    $("#round").toggleClass("collapse");
+    let creatureNumber = encounter.allCreatures.length;
+
+    for(let i = 0; i <= creatureNumber; i++){
+        if (encounter.allCreatures[i]._hp <= 0){
+            encounter.creatureRemove(i);
+            i--;
+        };
+        print();
+    };
+   
+};
+
+function loadItem(index){
+    let encounterList = localStorage.getItem("DREncounterTracker");
+    encounterList = JSON.parse(encounterList);
+    let item = encounterList[index];
+
+    encounter.allCreatures.length = 0;
+    let ItemName = localStorage.getItem(item + "name");
+    ItemName = JSON.parse(ItemName);
+    let ItemAC = localStorage.getItem(item + "ac");
+    ItemAC = JSON.parse(ItemAC);
+    let ItemMaxHp = localStorage.getItem(item + "maxhp");
+    ItemMaxHp = JSON.parse(ItemMaxHp);
+    let ItemHp = localStorage.getItem(item + "hp");
+    ItemHp = JSON.parse(ItemHp);
+    let ItemInit = localStorage.getItem(item + "init");
+    ItemInit = JSON.parse(ItemInit);
+    let ItemHostile = localStorage.getItem(item + "hostile");
+    ItemHostile = JSON.parse(ItemHostile);
+    let ItemConcentration = localStorage.getItem(item + "concentration");
+    ItemConcentration = JSON.parse(ItemConcentration);
+
+    for(let i = 0; i < Number(ItemName.length); i++) {
+        let creature = new Creature(ItemName[i], ItemAC[i], ItemMaxHp[i], ItemHp[i], ItemInit[i], ItemHostile[i], ItemConcentration[i]);
+        encounter.creatureToInit(creature);
+    };
+    
+
+    print();
+
+    /*let loadEncounter = localStorage.getItem("'"+value+"'");
+    loadEncounters = JSON.parse(loadEncounter);*/
+
+};
